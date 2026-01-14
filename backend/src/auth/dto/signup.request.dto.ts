15bbef93b33b014073942469ae55a1e6adb587d0
Park Jun Ho <ra___be@naver.com>
2026-01-14 21:10:56 +0900

    feat: 회원가입 DTO 정의 (#75)
    회원가입 요청/응답 데이터 전송 객체 구현:
    - SignupRequestDto: 이메일, 비밀번호, 닉네임, 약관 동의 검증
    - SignupResponseDto: 회원가입 성공 응답
    - class-validator를 통한 유효성 검증 규칙 적용

---
+++ backend/src/auth/dto/signup.request.dto.ts
@@ -0,0 +1,38 @@
+import { Equals, IsBoolean, IsEmail, IsNotEmpty, IsString, Matches, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+
+class AgreementsDto {
+  @IsBoolean()
+  @Equals(true)
+  termsOfService: boolean;
+
+  @IsBoolean()
+  @Equals(true)
+  privacyPolicy: boolean;
+
+  @IsBoolean()
+  marketingConsent: boolean;
+}
+
+export class SignupRequestDto {
+  @IsEmail({}, { message: "올바른 이메일 형식이 아닙니다." })
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[A-Za-z0-9!@#$%^&*]{8,32}$/, {
+    message: "비밀번호는 8~32자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.",
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[가-힣a-zA-Z0-9]{2,12}$/, { message: "닉네임은 2~12자 한글, 영문, 숫자만 사용할 수 있습니다." })
+  nickname: string;
+
+  @ValidateNested()
+  @Type(() => AgreementsDto)
+  @IsNotEmpty()
+  agreements: AgreementsDto;
+}
