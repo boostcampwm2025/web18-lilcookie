8d65ea065268435d614f54044b7dd4b055d76c75
sujin0530 <lucy4907@naver.com>
2026-01-15 13:38:32 +0900

    feat: AuthProvider 적용 및 인증 라우팅 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,33 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/web01" replace />} />
-        <Route path="/:teamId" element={<Dashboard />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* 루트 경로 - 로그인 페이지로 리다이렉트 */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* 인증 관련 라우트 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* 보호된 라우트 - 대시보드 */}
+          <Route
+            path="/:teamId"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
