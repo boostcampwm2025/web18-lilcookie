4a7fe5cbe70f0d2214c5ecd4643a9209df73d735
unhhyyeexx <unhhyyee66@gmail.com>
2026-01-22 18:55:25 +0900

    chore: user module import 추가

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AiController } from "./ai.controller";
 import { AiService } from "./ai.service";
 import { OidcModule } from "../oidc/oidc.module";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-  imports: [OidcModule],
+  imports: [OidcModule, UserModule],
   controllers: [AiController],
   providers: [AiService],
 })
