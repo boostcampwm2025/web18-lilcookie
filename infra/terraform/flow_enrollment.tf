749a175544fa8d2fcae360c877552ee35570af00
eupthere <eupthere@gmail.com>
2026-01-26 01:29:08 +0900

    feat: enrollment flow에 사용자 작성 및 이메일 검증 stage 추가 (#145)

--- infra/terraform/flow_enrollment.tf
+++ infra/terraform/flow_enrollment.tf
@@ -12,3 +12,14 @@ resource "authentik_flow_stage_binding" "teamstash_enrollment_prompt_binding" {
   order  = 10
 }
 
+resource "authentik_flow_stage_binding" "teamstash_enrollment_user_write_binding" {
+  target = authentik_flow.teamstash_enrollment_flow.uuid
+  stage  = authentik_stage_user_write.teamstash_user_write_stage.id
+  order  = 20
+}
+
+resource "authentik_flow_stage_binding" "teamstash_enrollment_email_verification_binding" {
+  target = authentik_flow.teamstash_enrollment_flow.uuid
+  stage  = authentik_stage_email.email_account_verification.id
+  order  = 30
+}
