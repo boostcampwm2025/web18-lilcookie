b266534ee2e1706b657d375a0dcb6cbafb60541e
eupthere <eupthere@gmail.com>
2026-01-26 01:28:06 +0900

    refactor: 사용자 식별 단계 위치 변경 (#145)
    - 알파벳 오름차순으로 정렬

--- infra/terraform/stages.tf
+++ infra/terraform/stages.tf
@@ -12,6 +12,12 @@ resource "authentik_stage_email" "email_account_verification" {
   recovery_cache_timeout   = "minutes=10"
 }
 
+# --- Identification Stages ---
+resource "authentik_stage_identification" "teamstash_identification_stage" {
+  name        = "TeamStash Identification Stage"
+  user_fields = ["username", "email"]
+}
+
 # --- Prompt Stages ---
 resource "authentik_stage_prompt" "teamstash_enrollment_prompt" {
   name = "Teamstash Enrollment Prompt"
@@ -24,22 +30,16 @@ resource "authentik_stage_prompt" "teamstash_enrollment_prompt" {
   ]
 }
 
-# --- Identification Stages ---
-resource "authentik_stage_identification" "teamstash_identification_stage" {
-  name        = "TeamStash Identification Stage"
-  user_fields = ["username", "email"]
-}
-
-# --- User Logout Stages ---
-resource "authentik_stage_user_logout" "post_logout_logout_stage" {
-  name = "post-logout-user-logout-stage"
-}
-
 # --- Redirect Stages ---
 resource "authentik_stage_redirect" "post_logout_dummy_redirect" {
   name          = "post-logout-dummy-redirect"
   mode          = "static"
   target_static = "about:blank"
+}
+
+# --- User Logout Stages ---
+resource "authentik_stage_user_logout" "post_logout_logout_stage" {
+  name = "post-logout-user-logout-stage"
 }
 
 # --- User Write Stages ---
