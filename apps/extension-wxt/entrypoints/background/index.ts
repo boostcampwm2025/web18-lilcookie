b3fb8451aeda2c2d6eeb19614f85c738d5d665dc
eupthere <eupthere@gmail.com>
2026-01-28 23:29:23 +0900

    refactor: 메시지 핸들러 구조 개선 (#178)
    - switch 문을 사용하여 메시지 핸들러를 개선함
    - 코드 가독성을 높이고 유지보수를 용이하게 함

--- apps/extension-wxt/entrypoints/background/index.ts
+++ apps/extension-wxt/entrypoints/background/index.ts
@@ -1,6 +1,9 @@
 import { login, logout, getAuthState } from "./auth.background";
 import { summarizeContent, saveLink } from "./messaging.background";
-import { setupAlarms, setupNotificationHandlers } from "./notifications.background";
+import {
+  setupAlarms,
+  setupNotificationHandlers,
+} from "./notifications.background";
 import { setupTabListeners } from "./tabs.background";
 
 export default defineBackground(() => {
@@ -9,25 +12,26 @@ export default defineBackground(() => {
   setupTabListeners();
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "saveLink") {
-      saveLink(request.data).then(sendResponse);
-      return true;
-    } else if (request.action === "summarize") {
-      summarizeContent(request.content)
-        .then(sendResponse)
-        .catch((error) =>
-          sendResponse({ success: false, error: error.message }),
-        );
-      return true;
-    } else if (request.action === "login") {
-      login().then(sendResponse);
-      return true;
-    } else if (request.action === "logout") {
-      logout().then(() => sendResponse({ success: true }));
-      return true;
-    } else if (request.action === "getAuthState") {
-      getAuthState().then(sendResponse);
-      return true;
+    switch (request.action) {
+      case "saveLink":
+        saveLink(request.data).then(sendResponse);
+        return true;
+      case "summarize":
+        summarizeContent(request.content)
+          .then(sendResponse)
+          .catch((error) =>
+            sendResponse({ success: false, error: error.message }),
+          );
+        return true;
+      case "login":
+        login().then(sendResponse);
+        return true;
+      case "logout":
+        logout().then(() => sendResponse({ success: true }));
+        return true;
+      case "getAuthState":
+        getAuthState().then(sendResponse);
+        return true;
     }
   });
 });
